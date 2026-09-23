ed6bdc1214f10ab379e0dc7c082cf33ec7efe159
cittyhello23-pixel <cittyhello23@gmail.com>
2026-09-23 10:09:32 +0200

    added the background image and pre existing html from the previous repository. This includes the main page and topic page, and the divs/boxes inside of these. i havent added any of the text yet, so theyre not visible to me, AKA i dont know if it works yet. I wrote some css for the hero background image to fit and have a linear gradient, aswell as the two main fonts. i used AI (with permission) to impliment live reload to the localhost.

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require("express");
+const livereload = require('livereload');
+const connectLivereload = require('connect-livereload');
+
+const liveReloadServer = livereload.createServer();
+liveReloadServer.watch('public'); // Watch frontend directory
+
 
 const app = express();
-
+app.use(connectLivereload());
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({extended:true}));
@@ -10,6 +16,14 @@ app.use(express.static("public"));
 app.get("/", (req, res) => {
     res.render("index")
 })
+
+
+// Refresh browser after Nodemon restarts the server
+liveReloadServer.server.once("connection", () => {
+  setTimeout(() => {
+    liveReloadServer.refresh("/");
+  }, 100);
+});
 
 app.listen(4000,() => {
     console.log("http://localhost:4000")

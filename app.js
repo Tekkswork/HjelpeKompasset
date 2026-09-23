a6cfe0672c8add261ab5138bf2cb3d24219a1c12
Hadidja Gagieva <gagijeva09@gmail.com>
2026-09-23 08:17:26 +0200

    prøver å pushe ut

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("index")
+})
+
+app.listen(4000,() => {
+    console.log("http://localhost:4000")
+});
+
